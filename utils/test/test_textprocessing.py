46a12bce1c0c62c9fcecb2acd20f53a44aff7f59
vancanhuit <vancanhuit@gmail.com>
2018-05-16 15:30:15 +0700

    Clean text

--- utils/test/test_textprocessing.py
+++ utils/test/test_textprocessing.py
@@ -6,3 +6,7 @@ class TestTextProcessing(object):
         text = 'áàảãạ âấầẩẫậ ăẵẳằắặ éèẻẽẹ êếềệểễ íìịỉĩ óòỏõọ ôốồổỗộ ơớờởỡợ úùủũụ ưứừửữự ýỳỵỷỹ'
         new_text = textprocessing.remove_accents(text)
         assert new_text == 'aaaaa aaaaaa aaaaaa eeeee eeeeee iiiii ooooo oooooo oooooo uuuuu uuuuuu yyyyy'
+
+    def test_clean_text(self):
+        text = 'Mỗi lít xăng sẽ chịu thuế môi trường 4.000 đồng còn dầu là 2.000 đồng, cả hai đều là mức cao nhất theo khung hiện hành'
+        print(textprocessing.clean_text(text.lower()))
--- utils/textprocessing.py
+++ utils/textprocessing.py
@@ -1,6 +1,12 @@
 import unicodedata
+import re
 
 
 def remove_accents(text):
     # https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-in-a-python-unicode-string
     return ''.join((c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn'))
+
+
+def clean_text(text):
+    pattern = pattern = re.compile(r'[^áàảãạâấầẩẫậăẵẳắằặđéèẻẽẹêếềểễệíìịỉĩóòõỏọôốồổộỗơớờởỡợúùũủụưứừửữựýỳỷỹỵ\sa-z]')
+    return re.sub(pattern, '', text)

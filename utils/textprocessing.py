7cc6ddf051c8cd813dae5f0e20b2d2dfafc5e7fd
vancanhuit <vancanhuit@gmail.com>
2018-05-16 15:01:13 +0700

    Link to solution on stackoverflow

--- utils/textprocessing.py
+++ utils/textprocessing.py
@@ -2,4 +2,5 @@ import unicodedata
 
 
 def remove_accents(text):
+    # https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-in-a-python-unicode-string
     return ''.join((c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn'))

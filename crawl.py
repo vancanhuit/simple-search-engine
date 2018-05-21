2a305d6b43f16dd361bb9eb05b38e5fc5ec7a579
vancanhuit <vancanhuit@gmail.com>
2018-05-21 20:51:37 +0700

    Add soha crawler

--- crawl.py
+++ crawl.py
@@ -1,8 +1,21 @@
 import crawlers.vnexpress.crawler as vnexpress_crawler
+import crawlers.soha.crawler as soha_crawler
 import os
 import re
 import shutil
 import sys
+
+
+def fetch(dataset, dataset_path, prefix):
+    for index, data in enumerate(dataset):
+        filename = os.path.join(dataset_path, prefix + str(index))
+        url, text = data
+        if text != '':
+            with open(filename, mode='w', encoding='utf-8') as f:
+                print('=====')
+                print('Fetching url: {}'.format(url))
+                print('Writing to file: {}'.format(filename))
+                f.write(text)
 
 
 dataset_path = os.path.expanduser(sys.argv[1])
@@ -11,15 +24,12 @@ if os.path.isdir(dataset_path):
 
 os.mkdir(dataset_path)
 
-dataset = vnexpress_crawler.crawl()
-for index, data in enumerate(dataset):
-    filename = os.path.join(dataset_path, 'vnexpress-' + str(index))
-    url, text = data
-    if text != '':
-        with open(filename, mode='w') as f:
-            print('=====')
-            print('Fetching url: {}'.format(url))
-            print('Writing to file: {}'.format(filename))
-            f.write(text)
+print('===== Fetching vnexpress data =====')
+vnexpress_dataset = vnexpress_crawler.crawl()
+fetch(vnexpress_dataset, dataset_path, 'vnexpress-')
+print('Done.')
 
-print('Done')
+print('===== Fetching soha data =====')
+soha_dataset = soha_crawler.crawl()
+fetch(soha_dataset, dataset_path, 'soha-')
+print('Done.')

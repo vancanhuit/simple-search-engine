dd45a6b6b805379d084c7844c396737827e971db
vancanhuit <vancanhuit@gmail.com>
2018-05-30 11:55:22 +0700

    Merge some files from windows branch

--- crawlers/laodong/crawler.py
+++ crawlers/laodong/crawler.py
@@ -2,12 +2,14 @@ from crawlers.laodong import helper
 from crawlers import common
 
 
-def crawl():
+def crawl(visited_urls):
     sitemap_url = 'https://laodong.vn/sitemap.xml'
     sitemaps = common.extract_sitemaps(sitemap_url)
 
     for sitemap in sitemaps[:10]:
         urls = common.extract_urls_from_sitemap(sitemap)
         for url in urls[:10]:
-            text = helper.extract_text_from_url(url)
-            yield url, text
+            if url not in visited_urls:
+                visited_urls.add(url)
+                text = helper.extract_text_from_url(url)
+                yield url, text
--- crawlers/thanhnien/crawler.py
+++ crawlers/thanhnien/crawler.py
@@ -3,9 +3,11 @@ from crawlers import common
 import requests
 
 
-def crawl():
+def crawl(visited_urls):
     sitemap_url = 'https://thanhnien.vn/sitemaps/newsindex.xml'
     urls = common.extract_urls_from_sitemap(sitemap_url)
     for url in urls[:100]:
-        text = helper.extract_text_from_url(url)
-        yield url, text
+        if url not in visited_urls:
+            visited_urls.add(url)
+            text = helper.extract_text_from_url(url)
+            yield url, text

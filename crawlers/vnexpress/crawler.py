5d6c7648a549bf2f55530c9ff0d84bb67a45fe88
vancanhuit <vancanhuit@gmail.com>
2018-06-03 14:57:04 +0700

    Update from windows branch

--- crawlers/vnexpress/crawler.py
+++ crawlers/vnexpress/crawler.py
@@ -5,9 +5,9 @@ from crawlers import common
 def crawl(visited_urls):
     sitemap_url = 'https://vnexpress.net/sitemap/1000000/sitemap.xml'
     sitemaps = common.extract_sitemaps(sitemap_url)
-    for sitemap in sitemaps[:10]:
+    for sitemap in sitemaps[:2]:
         urls = common.extract_urls_from_sitemap(sitemap)
-        for url in urls[:10]:
+        for url in urls[:50]:
             if url not in visited_urls:
                 visited_urls.add(url)
                 text = helper.extract_text_from_url(url)
